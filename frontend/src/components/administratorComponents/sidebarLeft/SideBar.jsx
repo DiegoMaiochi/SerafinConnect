69ca89e30bee3f124c1ca88bc272ab42de0eb4e5
Diego Maiochi <diegoabreu382@gmail.com>
2025-02-26 14:27:23 -0300

    feat: ajusta tamanho e cor dos icones da barralateral

--- frontend/src/components/administratorComponents/sidebarLeft/SideBar.jsx
+++ frontend/src/components/administratorComponents/sidebarLeft/SideBar.jsx
@@ -1,15 +1,17 @@
 import { FiUser } from "react-icons/fi";
 import { FiHome } from "react-icons/fi";
 import { FiPackage } from "react-icons/fi";
+import { FiSettings } from "react-icons/fi";
 
 export function SideBar(){
     return(
         <div>
             <div>
                 <div>
-                    <FiHome size={24} color="black" />
-                    <FiUser size={24} color="black" />
-                    <FiPackage size={24} color="black" />
+                    <FiHome size={50} color="#E6523A" />
+                    <FiUser size={50} color="#E6523A" />
+                    <FiPackage size={50} color="#E6523A" />
+                    <FiSettings size={50} color="#E6523A" />
                 </div>
             </div>
         </div>
