ca4b50de911d9bf8850b6d3f0770ba5d43aebfd5
Diego Maiochi <diegoabreu382@gmail.com>
2025-02-26 14:19:31 -0300

    feat/adiciona-icones-barra-lateral

--- frontend/src/components/administratorComponents/sidebarLeft/SideBar.jsx
+++ frontend/src/components/administratorComponents/sidebarLeft/SideBar.jsx
@@ -1,7 +1,17 @@
+import { FiUser } from "react-icons/fi";
+import { FiHome } from "react-icons/fi";
+import { FiPackage } from "react-icons/fi";
+
 export function SideBar(){
     return(
         <div>
-            
+            <div>
+                <div>
+                    <FiHome size={24} color="black" />
+                    <FiUser size={24} color="black" />
+                    <FiPackage size={24} color="black" />
+                </div>
+            </div>
         </div>
     )
 }
