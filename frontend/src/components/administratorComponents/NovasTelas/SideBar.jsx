d9daf0b0b4d08044b4607d51dfb25c374a8339ed
Diego Maiochi <diegoabreu382@gmail.com>
2025-03-12 14:14:07 -0300

    feat: adiciona interação entre páginas

--- frontend/src/components/administratorComponents/NovasTelas/SideBar.jsx
+++ frontend/src/components/administratorComponents/NovasTelas/SideBar.jsx
@@ -1,34 +1,46 @@
-import { FiCheckSquare, FiGrid, FiMonitor, FiPercent, FiSquare, FiTable, FiTag, FiUser } from "react-icons/fi";
-import { FiHome } from "react-icons/fi";
-import { FiPackage } from "react-icons/fi";
-import { FiSettings } from "react-icons/fi";
+import { useState } from "react";
+import { FiCheckSquare, FiHome, FiPackage, FiSettings, FiTag, FiUser } from "react-icons/fi";
 import { Dashboard } from "./Dashboard";
 import { Usuarios } from "./Usuarios";
 import { Produtos } from "./Produtos";
 import { Cupons } from "./Cupons";
 import { Mesas } from "./Mesas";
 
-export function SideBar(){
-    return(
+export function SideBar() {
+    const [activeComponent, setActiveComponent] = useState("Dashboard");
+
+    const renderComponent = () => {
+        switch (activeComponent) {
+            case "Dashboard":
+                return <Dashboard />;
+            case "Usuarios":
+                return <Usuarios />;
+            case "Produtos":
+                return <Produtos />;
+            case "Cupons":
+                return <Cupons />;
+            case "Mesas":
+                return <Mesas />;
+            default:
+                return <Dashboard />;
+        }
+    };
+
+    return (
         <div className="flex">
             <div className="bg-gray-200 w-24 h-screen flex flex-col justify-between relative">
                 <div className="space-y-5">
-                    <FiHome size={50} className=" text-red-500 mx-auto mt-4"/>
-                    <FiUser size={50} className=" text-red-500 mx-auto"/>
-                    <FiPackage size={50} className=" text-red-500 mx-auto"/>
-                    <FiTag size={50} className=" text-red-500 mx-auto"/>
-                    <FiCheckSquare size={50} className=" text-red-500 mx-auto"/>
+                    <FiHome size={50} className="text-red-500 mx-auto mt-4 cursor-pointer" onClick={() => setActiveComponent("Dashboard")} />
+                    <FiUser size={50} className="text-red-500 mx-auto cursor-pointer" onClick={() => setActiveComponent("Usuarios")} />
+                    <FiPackage size={50} className="text-red-500 mx-auto cursor-pointer" onClick={() => setActiveComponent("Produtos")} />
+                    <FiTag size={50} className="text-red-500 mx-auto cursor-pointer" onClick={() => setActiveComponent("Cupons")} />
+                    <FiCheckSquare size={50} className="text-red-500 mx-auto cursor-pointer" onClick={() => setActiveComponent("Mesas")} />
                 </div>
-                <FiSettings size={50} className=" text-red-500 mx-auto mb-4"/>
+                <FiSettings size={50} className="text-red-500 mx-auto mb-4" />
             </div>
-            <div className="w-full">
-                {/* para visualizar a página do Dashboard basta substituir o arquivo para o arquivo Dashboard */}
-                {/* para visualizar a página do Usuários basta substituir o arquivo para o arquivo Usuários */}
-                {/* para visualizar a página do Produtos basta substituir o arquivo para o arquivo Produtos */}
-                {/* para visualizar a página do Cupons basta substituir o arquivo para o arquivo Cupons */}
-                {/* para visualizar a página do Mesas basta substituir o arquivo para o arquivo Mesas */}
-                <Mesas/>
+            <div className="teste w-full">
+                {renderComponent()}
             </div>
         </div>
-    )
+    );
 }
