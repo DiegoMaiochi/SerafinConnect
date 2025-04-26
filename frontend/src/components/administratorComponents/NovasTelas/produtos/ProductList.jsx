fd5956ee26652e833b81431143f172924d917a90
Diego Maiochi <diegoabreu382@gmail.com>
2025-04-26 09:44:01 -0300

    feat: adiciona tela e prepara para edicao de produtos

--- frontend/src/components/administratorComponents/NovasTelas/produtos/ProductList.jsx
+++ frontend/src/components/administratorComponents/NovasTelas/produtos/ProductList.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { FiXCircle } from "react-icons/fi";
 
 export default function ProductList({ products }) {
     const [currentPage, setCurrentPage] = useState(1);
+    const [selectedProduct, setSelectedProduct] = useState(null);
     const productsPerPage = 18;
 
     const totalPages = Math.ceil(products.length / productsPerPage);
@@ -9,15 +11,29 @@ export default function ProductList({ products }) {
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
     const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
+    const toggleForm = () => setShowForm(!showForm);
+
     const handlePageChange = (pageNumber) => setCurrentPage(pageNumber);
 
+    const handleProductClick = (product) => {
+        setSelectedProduct(product);
+    };
+
+    const handleCloseSidebar = () => {
+        setSelectedProduct(null);
+    };
+
     return (
-        <div className="bg-gray-200 h-full mt-4 p-4 rounded-2xl">
+        <div className="relative bg-gray-200 h-full mt-4 p-4 rounded-2xl">
             {currentProducts.length > 0 ? (
                 <>
                     <ul className="grid grid-cols-6 gap-4">
                         {currentProducts.map((product) => (
-                            <li key={product.id} className="bg-gray-400 p-4 rounded-xl shadow-md">
+                            <li
+                                key={product.id}
+                                className="bg-gray-400 p-4 rounded-xl shadow-md cursor-pointer hover:bg-gray-500 transition"
+                                onClick={() => handleProductClick(product)}
+                            >
                                 <p><strong>Nome:</strong> {product.name}</p>
                                 <p><strong>EAN:</strong> {product.ean}</p>
                                 <p><strong>Preço:</strong> R$ {product.price}</p>
@@ -40,6 +56,25 @@ export default function ProductList({ products }) {
                 </>
             ) : (
                 <p>Nenhum produto cadastrado.</p>
+            )}
+
+            {selectedProduct && (
+                <div className="fixed top-0 right-0 h-full w-96 bg-gray-300 shadow-2xl p-6 z-50 flex flex-col">
+                    <button className="text-2xl text-black ml-2 self-end hover:bg-gray-300 hover:text-gray-500"
+                        onClick={handleCloseSidebar}>
+                        <FiXCircle size={30} />
+                    </button>
+
+                    <h2 className="text-2xl font-bold flex justify-center m-2">Detalhes do Produto</h2>
+
+                    <div className="bg-gray-400 p-4 rounded-xl shadow-md mt-12">
+                        <p><strong>Nome:</strong> {selectedProduct.name}</p>
+                        <p><strong>EAN:</strong> {selectedProduct.ean}</p>
+                        <p><strong>Preço:</strong> R$ {selectedProduct.price}</p>
+                        <p><strong>Quantidade:</strong> {selectedProduct.quantity}</p>
+                    </div>
+                    
+                </div>
             )}
         </div>
     );
